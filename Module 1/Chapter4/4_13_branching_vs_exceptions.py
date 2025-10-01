a78622d0e49159b82caca0679e81ea208c3d2ffc
Sotiris Riniou <liquidlight.gr@mac.com>
2025-10-01 22:26:00 +0300

    update chapter 4_13 using f string, 2decimal

--- Module 1/Chapter4/4_13_branching_vs_exceptions.py
+++ Module 1/Chapter4/4_13_branching_vs_exceptions.py
@@ -1,7 +1,6 @@
 def divide_with_exception(number, divisor):
-    try:
-        print("{} / {} = {}".format(
-            number, divisor, number / divisor * 1.0))
+    try: # use f-string
+        print(f"{number} / {divisor} = {number / divisor}")
     except ZeroDivisionError:
         print("You can't divide by zero")
 
@@ -9,10 +8,11 @@ def divide_with_if(number, divisor):
     if divisor == 0:
         print("You can't divide by zero")
     else:
-        print("{} / {} = {}".format(
-            number, divisor, number / divisor * 1.0))
+        # use f-string with format specifier to show 2 decimal places
+        # Equivalent to "{:.2f}".format(number / divisor)
+        print(f"{number} / {divisor} = {number / divisor:.2f}")
 
 divide_with_exception(10, 5)
-divide_with_if(10, 5)
+divide_with_if(23, 6)
 divide_with_if(10, 0)
 divide_with_exception(10, 0)
